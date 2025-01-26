715b9ba3a856a831d16bd38e2110b11cb118ea96
vivek-panchal <vivek.nsut@gmail.com>
2025-01-26 21:13:21 +0530

    implement Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
+import Navbar from "./components/navbar.component";
 const App = () => {
     return (
-        <h1 className=" bg-black text-white text-2xl flex items-center justify-center">Chad Blogs</h1>
+        <Navbar/>
     )
 }
 
